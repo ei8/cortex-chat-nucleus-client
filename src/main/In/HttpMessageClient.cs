4bdd33d1e4f9bf3f958c65195b16a6eb6e0f69de
elmerbool <35117496+elmerbool@users.noreply.github.com>
2024-01-16 19:30:59 +1100

    Updated TargetFramework and Polly reference.

--- src/main/In/HttpMessageClient.cs
+++ src/main/In/HttpMessageClient.cs
@@ -1,6 +1,7 @@
 ﻿using neurUL.Common.Http;
 using NLog;
 using Polly;
+using Polly.Retry;
 using Splat;
 using System;
 using System.Threading;
@@ -12,7 +13,7 @@ namespace ei8.Cortex.Chat.Nucleus.Client.In
     {
         private readonly IRequestProvider requestProvider;
 
-        private static Policy exponentialRetryPolicy = Policy
+        private static AsyncRetryPolicy exponentialRetryPolicy = Policy
             .Handle<Exception>()
             .WaitAndRetryAsync(
                 3,
